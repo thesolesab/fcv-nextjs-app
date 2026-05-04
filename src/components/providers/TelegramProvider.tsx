c3f4133cc8ea3df7da396e1ec949e12604edb295
R&L <thesolesab@gmail.com>
2026-05-04 12:09:16 +0300

    feat: implement TelegramProvider for WebApp integration and user data access

--- src/components/providers/TelegramProvider.tsx
+++ src/components/providers/TelegramProvider.tsx
@@ -3,20 +3,17 @@
 import { createContext, useContext, useEffect, useState, ReactNode } from 'react';
 import WebApp from '@twa-dev/sdk';
 import { TelegramUser } from '@/types/telegram';
-import { usePathname } from 'next/navigation';
 
 interface TelegramContextType {
   initData: string;
   user: TelegramUser | null;
   isReady: boolean;
-  startParam?: string;
 }
 
 const TelegramContext = createContext<TelegramContextType>({
   initData: '',
   user: null,
   isReady: false,
-  startParam: undefined,
 });
 
 export function useTelegram() {
@@ -27,10 +24,6 @@ export function TelegramProvider({ children }: { children: ReactNode }) {
   const [isReady, setIsReady] = useState(false);
   const [initData, setInitData] = useState('');
   const [user, setUser] = useState<TelegramUser | null>(null);
-  const [startParam, setStartParam] = useState<string | undefined>();
-
-  const pathname = usePathname()
-  console.log(pathname);
 
   useEffect(() => {
     // Ждем монтирования на клиенте
@@ -41,9 +34,6 @@ export function TelegramProvider({ children }: { children: ReactNode }) {
         if (WebApp.initDataUnsafe?.user) {
           setUser(WebApp.initDataUnsafe.user as TelegramUser);
         }
-        if (WebApp.initDataUnsafe?.start_param) {
-          setStartParam(WebApp.initDataUnsafe.start_param);
-        }
       } catch (error) {
         console.error('Telegram WebApp error:', error);
       } finally {
@@ -53,7 +43,7 @@ export function TelegramProvider({ children }: { children: ReactNode }) {
   }, []);
 
   return (
-    <TelegramContext.Provider value={{ initData, user, isReady, startParam }}>
+    <TelegramContext.Provider value={{ initData, user, isReady }}>
       {children}
     </TelegramContext.Provider>
   );
